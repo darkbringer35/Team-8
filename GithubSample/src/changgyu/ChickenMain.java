178bf6123ef0fe98f6eded836355f398a1222e82
가족 <@DESKTOP-NG516K3>
2017-12-26 19:44:22 +0900

    dd

--- GithubSample/src/changgyu/ChickenMain.java
+++ GithubSample/src/changgyu/ChickenMain.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -26,19 +27,35 @@ public class ChickenMain {
 	public static class MyFrame extends JFrame implements ActionListener { // JFrame을 상속받는다
 
 		String[] prCode = new String[15]; // 제품코드번호를 저장할 변수 생성
-		JLabel msg = new JLabel("메세지 출력부"); // 메세지를 나타낼 레이블 생성
 
 		JPanel p1 = new JPanel(); 
 		JPanel p2 = new JPanel(); 
 		JPanel p3 = new JPanel(); 
 		JPanel p3_1 = new JPanel();
 		JPanel p3_2 = new JPanel();
+		JPanel p3_3 = new JPanel();
 		
-		JButton btn1 = new JButton("BTN1"); 
-		JButton btn2 = new JButton("BTN2"); 
-		JButton btn3 = new JButton("BTN3"); 
-		JButton btn4 = new JButton("BTN4"); 
-		JButton btn5 = new JButton("현금결제");
+		JButton btn1 = new JButton("재고관리"); 
+		JButton btn2 = new JButton("매출관리"); 
+		JButton btn3 = new JButton("테이블 편집"); 
+		JButton btn4 = new JButton("환경설정");
+		
+		JButton btn5 = new JButton("카드결제");
+		JButton btn6 = new JButton("현금결제");
+		JLabel lb1 = new JLabel("결제 금액");
+		JLabel lb2 = new JLabel("받은 금액");
+		JLabel lb3 = new JLabel("잔       돈");
+		JTextField t1 = new JTextField();
+		JTextField t2 = new JTextField();
+		JTextField t3 = new JTextField();
+		
+		JButton[]pad = new JButton[12];
+		
+		JButton btnPlus = new JButton("+");
+		JButton btnMinus = new JButton("-");
+		
+	
+		
 		JButton table = new JButton("table");
 		//JLabel table = new JLabel("table");
 	
@@ -47,25 +64,19 @@ public class ChickenMain {
 		boolean editmode; // 삽입인지 수정인지를 결정하는 플래그값 선언 false=생성, true=수정
 
 		public MyFrame() { // 프레임 선언
-
-			
 			
 			this.setLayout(new BorderLayout()); // BorderLayout으로 레이아웃 설정
-			setSize(1000, 700); // 실행창 크기 설정
+			setSize(1050, 720); // 실행창 크기 설정
 			setLocation(300, 100); // 실행창 위치설정
 			setTitle("치킨"); // 프레임 타이틀 설정
 			this.setBackground(Color.white);
-			
-			msg.setHorizontalAlignment(SwingConstants.CENTER); // 메세지의 위치를 가운데에 설정
-			msg.setVerticalAlignment(SwingConstants.CENTER);
-			this.add(msg, BorderLayout.NORTH); // 메세지를 레이아웃의 위쪽에 설정
 
 			p1.setLayout(null); // 패널1의 레이아웃을 null로 설정
 			p1.setPreferredSize(new Dimension(200, 500));
-			btn1.setBounds(0, 0, 200, 75); // 1의 위치를 설정
-			btn2.setBounds(0, 75, 200, 75); // 2의 위치를 설정
-			btn3.setBounds(0, 150, 200, 75); // 3의 위치를 설정
-			btn4.setBounds(0, 225, 200, 75); // 4의 위치를 설정
+			btn1.setBounds(0, 0, 200, 120); // 1의 위치를 설정
+			btn2.setBounds(0, 120, 200, 120); // 2의 위치를 설정
+			btn3.setBounds(0, 240, 200, 120); // 3의 위치를 설정
+			btn4.setBounds(0, 360, 200, 120); // 4의 위치를 설정
 			p1.add(btn1); // 1을 패널1에 추가
 			p1.add(btn2); // 2을 패널1에 추가
 			p1.add(btn3); // 3을 패널1에 추가
@@ -73,28 +84,76 @@ public class ChickenMain {
 
 
 			p2.setLayout(null); // 패널2의 레이아웃을 null로 설정
-			p2.setPreferredSize(new Dimension(800, 500));
+			//p2.setPreferredSize(new Dimension(200, 500));
+			p2.setPreferredSize(new Dimension(200, 240));
+			
 			p2.setBackground(Color.white);
 			p2.setBorder(new TitledBorder(new LineBorder(Color.black)));
 			table.setBounds(100,100,100,100);
 			p2.add(table);
+			//btnPlus.setBounds(755, 24, 30, );
+			//p2.add(btnPlus);
+			//p2.add(btnMinus);
 			
 	
 			p3.setLayout(null); // 패널3의 레이아웃을 null로 설정
-			p3.setPreferredSize(new Dimension(1000, 200));
+			p3.setPreferredSize(new Dimension(1000, 210));
 			p3.setBackground(Color.white);
-			p3_1.setBounds(0, 0, 500, 200);
+			p3_1.setBounds(0, 0, 375, 240);
 			p3_1.setBackground(Color.white);
 			
-			btn5.setBounds(550, 0, 50, 50);
+
+			p3_1.setLayout(null); //패널 3-1 레이아웃
+			lb1.setBounds(25,24,100,24);
+			p3_1.add(lb1);
+			lb2.setBounds(25,72,100,24);
+			p3_1.add(lb2);
+			lb3.setBounds(25,120,100,24);
+			p3_1.add(lb3);
+			t1.setBounds(125,24,175,24);
+			p3_1.add(t1);
+			t2.setBounds(125,72,175,24);
+			p3_1.add(t2);
+			t3.setBounds(125,120,175,24);
+			p3_1.add(t3);
+			btn5.setBounds(25,168, 125, 24);
 			p3_1.add(btn5);
+			btn6.setBounds(200,168,125,24);
+			p3_1.add(btn6);
+		
 			
-			p3_2.setBounds(500, 0, 500, 200);
-			p3_2.setBackground(Color.black);
+			
+			p3_2.setBounds(375,0,375,210);
+			p3_2.setBackground(Color.white);
+			p3_2.setLayout(new GridLayout(4,3));
+			//p3_2.setPreferredSize(new Dimension(375,200));
+			for(int i=0; i<12; i++) {
+				if(i==9) {
+					pad[i] = new JButton("00");
+				}
+				else if(i==10) {
+					pad[i] = new JButton("0");
+				}
+				else if(i==11){
+					pad[i] = new JButton("C");
+				}
+				else {
+					pad[i] = new JButton(""+(i+1));
+				}
+				//pad[i].setSize(125, 40);
+				p3_2.add(pad[i]);
+				
+			}
+			
+			
+			p3_3.setBounds(750,0,300,240);
+			p3_3.setBackground(Color.black);
+			
 			
 			
 			p3.add(p3_1);
 			p3.add(p3_2);
+			p3.add(p3_3);
 			this.add(p1, BorderLayout.WEST); // 패널1을 왼쪽에 붙인다
 			this.add(p2, BorderLayout.CENTER); // 패널2를 가운데에 붙인다
 			this.add(p3, BorderLayout.SOUTH); // 패널3을 아래쪽에 붙인다
